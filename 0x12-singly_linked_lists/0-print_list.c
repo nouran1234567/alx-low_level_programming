71d7991c35ae98103c81703ac9862c8df423421f
nouran1234567 <nouran.a.mohamed23@gmail.com>
2023-07-28 16:55:46 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s= 0;
+	size_t m = 0;
 
 	while (h)
 	{
@@ -19,9 +19,9 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		m++;
 	}
 
-	return (s);
+	return (m);
 }
 
