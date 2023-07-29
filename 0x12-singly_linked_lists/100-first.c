e8bf58159bf9e7896a1e6e6274d335108aa250ad
nouran1234567 <nouran.a.mohamed23@gmail.com>
2023-07-28 18:10:42 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - function that run before the main function
+ *
+ * function is executed
+ *
+ * Return: void
+ */
+void _constuctor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
