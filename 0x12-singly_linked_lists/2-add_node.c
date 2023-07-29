bd8138dbd66dc23ef5c90b88411f727e576f6683
nouran1234567 <nouran.a.mohamed23@gmail.com>
2023-07-28 17:24:35 -0700

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len])
 		len++;
 
-	new = malloc(sizeof(list_t));
+	new_head = malloc(sizeof(list_t));
 	if (!new_head)
 		return (NULL);
 
