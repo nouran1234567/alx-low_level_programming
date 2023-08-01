29b3398ab1cc710c0f09d03c36d67609c8deff37
nouran1234567 <nouran.a.mohamed23@gmail.com>
2023-08-01 16:53:03 -0700

    10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	for (x = 0; temp-node && x < idx; x++)
+	for (x = 0; temp_node && x < idx; x++)
 	{
 		if (x == idx - 1)
 		{
