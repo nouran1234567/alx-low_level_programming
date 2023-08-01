a2b2ab2fed321df1a7425eab77765a9429b97ebc
nouran1234567 <nouran.a.mohamed23@gmail.com>
2023-08-01 16:19:23 -0700

    4

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,3 +34,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 listint_t *reverse_listint(listint_t **head);
 size_t free_listint_safe(listint_t **h);
 int _puchar(char c);
+
+#endif
+
