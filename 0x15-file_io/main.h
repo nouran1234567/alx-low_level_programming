6b99d7e885d37ea39dc59e40bad4e1024bd69bf1
nouran1234567 <nouran.a.mohamed23@gmail.com>
2023-08-07 19:05:09 -0700

    2

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,8 +5,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
