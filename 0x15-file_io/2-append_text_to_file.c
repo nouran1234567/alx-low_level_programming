4ebb55e096bddb6af39bca43bc88a81adc9feb14
nouran1234567 <nouran.a.mohamed23@gmail.com>
2023-08-07 19:38:46 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
-	wt = write(o, text_content, len);
+	wt = write(fd, text_content, len);
 
 	if (fd == -1 || wt == -1)
 		return (-1);
