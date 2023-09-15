2505348014622119a27f4a10211250e72975b9b1
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-14 22:50:08 -0700

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees dlistint_t lists.
+ *
+ * @head: pointer to list's top
+ *
+ * Return: no return
+ **/
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+	return;
+	while (head->next)
+	{
+	z = head
+	z = z->next;
+	free(z->prev);
+	}
+	free(z);
+}
