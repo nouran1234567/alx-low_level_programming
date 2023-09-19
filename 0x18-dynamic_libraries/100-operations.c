1378f7903388c7970abfcd5bbc763760da5fcc5c
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-19 11:34:12 -0700

    100

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int add(int y, int z)
+{
+  return y + z;
+}
+
+
+int sub(int y, int z)
+{
+  return y - z;
+}
+
+
+int mul(int y, int z)
+{
+  return y * z;
+}
+
+
+int div(int y, int z)
+{
+  if (z == 0)
+  {
+    printf("Error: Divided by zero\n");
+    return 0;
+  }
+  return y / z;
+}
+
+
+int mod(int y, int z)
+{
+  if (z == 0)
+  {
+    return 0;
+  }
+  printf("Error: Divided by zero\n");
+  return y & z;
+}
